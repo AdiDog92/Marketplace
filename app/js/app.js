7f0d6dd129d129b405f6c89c328edfb30d287ba9
AdiDog92 <adidog92@gmail.com>
2024-02-17 22:23:54 +0300

    тестовое

---
+++ app/js/app.js
@@ -0,0 +1,10 @@
+const checkValue = document.querySelectorAll(".sidebar__label");
+const checkbox = document.querySelectorAll(".sidebar__input");
+
+let arr = [];
+
+checkBox.forEach((element) => {
+  element.addEventListener("click", (e) => {
+
+  });
+});
